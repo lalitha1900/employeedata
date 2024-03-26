2aa2a417596b119f8998d625a5c0fb41c1ab9902
lalitha1900 <trlalitha2000@gmail.com>
2024-03-27 00:51:43 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [employees, setEmployees] = useState([]);
+  const [totalEmployeesData, setTotalEmployeesData] = useState([]);
+  const [counter, setCounter] = useState(0);
+  async function getEmployees() {
+    const response = await fetch(
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+      setTotalEmployeesData(data);
+      var newArr = [];
+      for (var i = 0; i < 10; i++) {
+        newArr.push(data[i]);
+      }
+      setEmployees(newArr);
+      setCounter(0);
+      //setEmployees(new Array(data).splice(0, 10));
+    }
+  }
+  useEffect(() => {
+    getEmployees();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Employee data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id + employee.name + new Date().getTime()}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              <td>{employee.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button
+          onClick={() => {
+            if (counter > 0) {
+              var temp = counter - 1;
+              setCounter(counter - 1);
+              console.log(temp);
+              console.log(totalEmployeesData);
+              var newArr = [];
+              for (
+                var i = temp * 10;
+                i < Math.min(temp * 10 + 10, totalEmployeesData.length);
+                i++
+              ) {
+                newArr.push(totalEmployeesData[i]);
+              }
+              setTimeout(() => {
+                setEmployees(newArr);
+              }, 1);
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <div className="pageNo" style={{ display: "inline" }}>
+          {counter + 1}
+        </div>
+        <button
+          onClick={() => {
+            if (counter < parseInt(totalEmployeesData.length / 10)) {
+              var temp = counter + 1;
+              setCounter(counter + 1);
+              console.log(temp);
+              console.log(totalEmployeesData);
+              var newArr = [];
+              for (
+                var i = temp * 10;
+                i < Math.min(temp * 10 + 10, totalEmployeesData.length);
+                i++
+              ) {
+                newArr.push(totalEmployeesData[i]);
+              }
+              setTimeout(() => {
+                setEmployees(newArr);
+              }, 1);
+            }
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
